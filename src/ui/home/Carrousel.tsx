13d22045a4d1e7fc484f8d1e98a72da70fa40b31
Zeth <lizeth.sj.1b@gmail.com>
2025-11-13 14:16:34 -0600

    ✨ FEAT: Promotions Modal Created

---
+++ src/ui/home/Carrousel.tsx
@@ -0,0 +1,39 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/ui/shared/carousel"
+
+import Image1 from '@/assets/images/1.png'
+import Image2 from '@/assets/images/2.png'
+import Image3 from '@/assets/images/3.png'
+import Image4 from '@/assets/images/4.png'
+import Image5 from '@/assets/images/5.png'
+
+const images = [Image1, Image2, Image3, Image4, Image5];
+
+export function Carrousel() {
+  return (
+    <div>
+      <Carousel opts={{ align: "start" }}>
+        <CarouselContent>
+          {images.map((img, index) => (
+            <CarouselItem key={index}>
+              <div className="h-auto place-items-center">
+                <img
+                  src={img.src}
+                  alt={`Slide ${index + 1}`}
+                  className=" md:h-100 lg:h-150"
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="p-5 md:p-6 bg-primary">Prev</CarouselPrevious>
+        <CarouselNext className="p-5 md:p-6 bg-primary">Next</CarouselNext>
+      </Carousel>
+    </div>
+  )
+}
