38f23c62b989a7db6be274c14b6c5fa51c2ecb8f
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-11-20 16:12:52 -0600

    Fix modal in mobile

--- src/ui/home/Modal.tsx
+++ src/ui/home/Modal.tsx
@@ -1,30 +1,33 @@
+import { X } from "lucide-react";
+import { Carrousel } from "@/ui/home/Carrousel";
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/ui/shared/alert-dialog"
-import { Carrousel } from "@/ui/home/Carrousel"
-import { X } from "lucide-react"
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/ui/shared/alert-dialog";
 
 export function AlertDialogDemo() {
-  return (
-    <AlertDialog defaultOpen={true}>
-      <AlertDialogContent className="fixed z-300 max-w-2xs md:max-w-lg lg:max-w-2xl">
-        <AlertDialogHeader>
-          <AlertDialogTitle className="text-4xl font-bold text-right mt-4">
-            <AlertDialogCancel className="text-base text-white bg-primary md:p-5"> <X /> </AlertDialogCancel>
-          </AlertDialogTitle>
-        </AlertDialogHeader>
-        <AlertDialogDescription className="m-8 md:m-10">
-          <Carrousel />                                      
-        </AlertDialogDescription>
-      </AlertDialogContent>
-    </AlertDialog>
-  )
+	return (
+		<AlertDialog defaultOpen={true}>
+			<AlertDialogContent className='fixed z-300 md:max-w-lg lg:max-w-2xl'>
+				<AlertDialogHeader>
+					<AlertDialogTitle className='text-4xl font-bold text-right mt-4'>
+						<AlertDialogCancel className='text-base text-white bg-primary md:p-5'>
+							{" "}
+							<X />{" "}
+						</AlertDialogCancel>
+					</AlertDialogTitle>
+				</AlertDialogHeader>
+				<AlertDialogDescription className='m-8 md:m-10'>
+					<Carrousel />
+				</AlertDialogDescription>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
