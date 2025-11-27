e9d06396d1a605358d7219a00eff722d0068afae
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-11-27 01:07:02 -0600

    Refactor elements in the page

--- src/ui/shared/massage-cards/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CardMassages } from "./massage-cards.astro";
--- src/ui/shared/services-cards/services-cards.tsx
+++ src/ui/shared/services-cards/services-cards.tsx
@@ -10,12 +10,12 @@ interface Props {
 
 export function CardServices({ title, text1, text2, text3 }: Props) {
 	return (
-		<Card className='border-primary h-full'>
+		<Card className='border-primary h-full bg-transparent overflow-hidden'>
 			<CardContent className='relative h-full flex flex-col'>
 				<CardHeader className='px-0 relative z-20'>
 					<CardTitle className='text-2xl font-bold'>{title}</CardTitle>
 				</CardHeader>
-				<div className='bg-primary-foreground p-7 absolute rounded-full -top-5 right-1'></div>
+				<div className='bg-primary-foreground absolute size-17.25 rounded-full -top-12.5 -right-6.5 -z-10'></div>
 
 				<ul className='text-lg mb-10'>
 					<li className='mb-2'>{text1}</li>
@@ -28,7 +28,7 @@ export function CardServices({ title, text1, text2, text3 }: Props) {
 						href='https://wa.link/dc586b'
 						target='_blank'
 						rel='noopener noreferrer'
-						className='w-full lg:w-auto block mt-auto text-lg px-4 py-3 border-1 border-primary rounded-lg hover:bg-primary-foreground hover:text-white transition-colors duration-300 ease-in-out'
+						className='w-full text-center mt-auto text-lg py-3 border-1 border-primary rounded-lg hover:bg-primary-foreground hover:text-white transition-colors duration-300 ease-in-out'
 					>
 						Saber más...
 					</a>
